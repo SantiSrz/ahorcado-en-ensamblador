2d7d19d52bbfa63c9b00be1d492c807564576b1d
santibox888@gmail.com <santibox888@gmail.com>
2026-02-25 17:48:00 +0100

    Issue 1.1: Configurar el entorno (Hello World)

---
+++ Ahorcado.asm
@@ -0,0 +1,24 @@
+section .data
+
+    mensaje db 'Hola Mundo', 0xA
+    longitud equ $ - mensaje
+
+section .text
+
+    global _start
+_start:
+
+    mov eax, 4
+    mov ebx, 1
+    call print
+
+    mov eax, 1
+    mov ebx, 0
+
+    int 0x80
+
+print:
+    mov ecx, mensaje
+    mov edx, longitud
+    int 0x80
+    ret
---
+++ makefile
@@ -0,0 +1,15 @@
+NAME = Ahorcado
+
+all: $(NAME)
+
+$(NAME): $(NAME).o
+	ld -m elf_i386 $(NAME).o -o $(NAME)
+
+$(NAME).o: $(NAME).asm
+	nasm -f elf32 $(NAME).asm -o $(NAME).o
+
+clean:
+	rm -f $(NAME) $(NAME).o
+
+run: all
+	./$(NAME)
