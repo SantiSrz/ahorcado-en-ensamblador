95292915ea0ec476520ee71f16c20d02bf8a9ae0
santibox888@gmail.com <santibox888@gmail.com>
2026-03-04 19:39:37 +0100

    Issue 2.1: Definir la memoria estática y dinámica (.data y .bss)

--- Ahorcado.asm
+++ Ahorcado.asm
@@ -1,24 +1,40 @@
 section .data
+    mensaje db 'Dime una letra:'
+    longitud_1 equ $ - mensaje
 
-    mensaje db 'Hola Mundo'
-    longitud equ $ - mensaje
+    palabra db '______'
+    longitud_2 equ $ - palabra
+
+    salto db 0x0A
+
+    solucion db 'MADRID'
+    longitud_3 equ $ - solucion
+
+section .bss
+    letra resb 1
+    espacio_restante resb 1
 
 section .text
-
     global _start
 _start:
+    mov ecx, mensaje
+    mov edx, longitud_1
+    call print
 
-    mov eax, 4
-    mov ebx, 1
+    mov ecx, palabra
+    mov edx, longitud_2
+    call print
+
+    mov ecx, salto
+    mov edx, 1
     call print
 
     mov eax, 1
     mov ebx, 0
-
     int 0x80
 
 print:
-    mov ecx, mensaje
-    mov edx, longitud
+    mov eax, 4
+    mov ebx, 1
     int 0x80
     ret
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-NAME = Caracter
+NAME = Ahorcado
 
 all: $(NAME)
 
