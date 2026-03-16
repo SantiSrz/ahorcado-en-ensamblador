a5192c452faea7ff665ffc3ce62ca63802cd7e18
lozanno00 <luis.lozano.sanz43@gmail.com>
2026-03-16 16:02:58 +0100

    Issue 4.1: Estructurar el bucle principal

--- Ahorcado.asm
+++ Ahorcado.asm
@@ -1,21 +1,15 @@
 section .data
-    mensaje db 'Dime una letra:'
+    mensaje db 'Dime una letra: '
     longitud_1 equ $ - mensaje
-
-    palabra db '______'
+    palabra db '______', 0x0A
     longitud_2 equ $ - palabra
-
     solucion db 'MADRID'
     longitud_3 equ $ - solucion
-
     intentos db 'Numero de intentos restantes: '
     longitud_4 equ $ - intentos
-
     numero_contador db ' ', 0x0A
     longitud_5 equ $ - numero_contador
-
     salto db 0x0A
-
     vidas db 6
 
 section .bss
@@ -26,28 +20,25 @@ section .text
     global _start
 
 _start:
-    mov ecx, mensaje
-    mov edx, longitud_1
-    call print
 
+game_loop:
     mov ecx, palabra
     mov edx, longitud_2
-    call print
-
-    mov al, byte [vidas]
-    add al, 48
-    mov [numero_contador], al
-
-    mov ecx, salto
-    mov edx, 1
     call print
 
     mov ecx, intentos
     mov edx, longitud_4
     call print
 
+    mov al, byte [vidas]
+    add al, 48
+    mov [numero_contador], al
     mov ecx, numero_contador
     mov edx, longitud_5
+    call print
+
+    mov ecx, mensaje
+    mov edx, longitud_1
     call print
 
     mov eax, 3
@@ -62,7 +53,6 @@ limpiar_buffer:
     mov ecx, basura
     mov edx, 1
     int 0x80
-
     mov al, byte [basura]
     cmp al, 0x0A
     jne limpiar_buffer
@@ -76,34 +66,41 @@ bucle_comparacion:
     mov al, byte [esi]
     cmp al, byte [letra]
     jne no_coincide
-
     mov byte [edi], al
     mov bl, 1
-
 no_coincide:
     inc esi
     inc edi
     loop bucle_comparacion
 
     cmp bl, 0
-    je fallo
-
-    jmp fin
-
-fallo:
+    jne verificar_fin
     dec byte [vidas]
+    jz fin
+
+verificar_fin:
+    mov ecx, longitud_3
+    mov edi, palabra
+    mov al, '_'
+    xor edx, edx
+revisar_guiones:
+    cmp byte [edi], al
+    jne letra_revelada
+    inc edx
+letra_revelada:
+    inc edi
+    loop revisar_guiones
+
+    cmp edx, 0
+    je fin
+    jmp game_loop
 
 fin:
     mov ecx, palabra
     mov edx, longitud_2
     call print
-
-    mov ecx, salto
-    mov edx, 1
-    call print
-
     mov eax, 1
-    mov ebx, 0
+    xor ebx, ebx
     int 0x80
 
 print:
